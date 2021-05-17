e157373cca45ecee6b70e127724ccb12285a970e
Kübra Şimşek <kubra.siimsek@outlook.com>
2021-05-17 11:18:36 +0300

    singlemessage

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMq.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("linkinizi buraya yazınız");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            //yoksa sıfırdan oluşturur varsa bir işlem yapmaz.
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"Message {x}";
+
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                Console.WriteLine($"mesaj gönderilmiştir : {message}");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMq.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("linkinizi buraya yazınız");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //Eğer publisher 'ın bunu tanımladığına eminseniz bunu silebilirsiniz.
+            //Eğer publisher da tanımlanmadıysa burda tanımlama yapılır.
+            //Her iki taraf için de tanımlama yapılacaksa parametreler aynı olmalı
+            ////channel.QueueDeclare("hello-queue", true, false, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj:" + message);
+            };
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
